c8b89944a3a891273c627cede9355964626c90d5
Ivan Lago <ivanslago@gmail.com>
2025-05-28 01:58:39 -0300

    add_shadcn_setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
-    <div>
+    <div className="flex flex-col items-center justify-center h-screen">
       <h1>CliniHora</h1>
+      <Button>Login</Button>
     </div>
   );
 }
